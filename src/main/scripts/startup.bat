da0465d759048b0e3aecf4e10ec3d71a677cfcf4
matsrogalski <mrogalski@mango-solutions.com>
2014-03-25 16:23:41 +0000

    DDMORE-614: Improvement so the working files are created within FIS home, not current users' directory. Now locations of pharmml to nm-tran converter and nonmem environment setup script are resolved assuming PKPD-stick directory structure.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -1,11 +1,16 @@
 SET FIS_HOME=%~dp0
 
 IF %FIS_HOME:~-1%==\ SET FIS_HOME=%FIS_HOME:~0,-1%
+
+CD %FIS_HOME%
+
 SET PUBLISH_INPUTS=%FIS_HOME%\scripts\publishInputs.groovy
 SET PUBLISH_OUTPUTS=%FIS_HOME%\scripts\retrieveOutputs.groovy
 
 SET params=-Dfis.publishInputs="%PUBLISH_INPUTS%"
 SET params=-Dfis.retrieveOutputs="%PUBLISH_OUTPUTS%" %params%
+SET params=-Dnonmem.setup.script="%~dp0\..\setup.bat" %params%
+SET params=-Dconverter.toolbox.executable="%~dp0\..\converter-pharmml2nmtran-0.0.3-SNAPSHOT\convert.bat" %params%
 
 reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul
 if %errorlevel%==0 (
