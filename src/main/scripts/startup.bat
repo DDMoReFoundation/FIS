989a4a5bb55ace0e68839591ef776953b807e833
Yannis Theocharis <ytheohar@gmail.com>
2014-03-25 15:56:01 +0000

    DDMORE-612 Java set up.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -7,4 +7,11 @@ SET PUBLISH_OUTPUTS=%FIS_HOME%\scripts\retrieveOutputs.groovy
 SET params=-Dfis.publishInputs="%PUBLISH_INPUTS%"
 SET params=-Dfis.retrieveOutputs="%PUBLISH_OUTPUTS%" %params%
 
-java %params% -Dmango.mif.invoker.shell="cmd.exe /C" -Dmango.mif.invoker.tmp.file.ext=".bat" -DFIS_HOME="%FIS_HOME%" -jar %FIS_HOME%\fis.jar
+reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul
+if %errorlevel%==0 (
+	set TRAINING_JAVA_HOME="..\..\MDL IDE\32bits\jre"
+) else (
+	set TRAINING_JAVA_HOME="..\..\MDL IDE\64bits\jre"
+)
+
+%TRAINING_JAVA_HOME%\bin\java %params% -Dmango.mif.invoker.shell="cmd.exe /C" -Dmango.mif.invoker.tmp.file.ext=".bat" -DFIS_HOME="%FIS_HOME%" -jar %FIS_HOME%\fis.jar
