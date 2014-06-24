fb0679ad6dc74e0a89d64eac3b730ea27ed8a8a4
Matthew Wise <mwise@mango-solutions.com>
2014-06-24 11:55:36 +0000

    DDMORE-497: Converter toolbox assembly binary Zip was renamed - update FIS startup.bat accordingly.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -10,7 +10,7 @@ SET PUBLISH_OUTPUTS=%FIS_HOME%\scripts\retrieveOutputs.groovy
 SET params=-Dfis.publishInputs="%PUBLISH_INPUTS%"
 SET params=-Dfis.retrieveOutputs="%PUBLISH_OUTPUTS%" %params%
 SET params=-Dnonmem.setup.script="%~dp0\..\setup.bat" %params%
-SET params=-Dconverter.toolbox.executable="%~dp0\..\converter-modules-aggregator-0.0.1-SNAPSHOT\convert.bat" %params%
+SET params=-Dconverter.toolbox.executable="%~dp0\..\converter-toolbox-bundle-0.0.1-SNAPSHOT\convert.bat" %params%
 
 reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul
 if %errorlevel%==0 (
