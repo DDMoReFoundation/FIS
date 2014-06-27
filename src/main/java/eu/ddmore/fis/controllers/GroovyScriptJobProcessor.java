61fa8bc66c5b808805009cdb3ad53b391eafe316
Matthew Wise <mwise@mango-solutions.com>
2014-06-27 13:33:55 +0000

    DDMORE-729: Formatting only, no functional changes

--- src/main/java/eu/ddmore/fis/controllers/GroovyScriptJobProcessor.java
+++ src/main/java/eu/ddmore/fis/controllers/GroovyScriptJobProcessor.java
@@ -5,17 +5,20 @@ import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import java.io.File;
 import org.springframework.beans.factory.annotation.Required;
+
 /**
  * Uses a groovy script to process job
  */
 public class GroovyScriptJobProcessor implements JobResourceProcessor {
+
     private File scriptFile;
-    
+
     private Binding binding;
-    
+
     public GroovyScriptJobProcessor() {
         binding = new Binding();
     }
+
     /**
      * Creates an instance which will pass the binding object to the groovy script execution context.
      * @param binding
@@ -23,15 +26,15 @@ public class GroovyScriptJobProcessor implements JobResourceProcessor {
     public GroovyScriptJobProcessor(Binding binding) {
         this.binding = binding;
     }
-    
+
     private GroovyShell createShell(Binding binding) {
         return new GroovyShell(this.getClass().getClassLoader(), binding);
     }
-    
+
     private Binding getBinding() {
         return new Binding(binding.getVariables());
     }
-    
+
     @Override
     public LocalJob process(LocalJob job) {
         Binding binding = getBinding();
@@ -41,25 +44,26 @@ public class GroovyScriptJobProcessor implements JobResourceProcessor {
         execute(shell, scriptFile);
         return (LocalJob) binding.getVariable("job");
     }
+
     /**
      * Executes a script in the given shell
      * @param shell
-     * @param script
+     * @param scriptFile
      * @return result of the script evaluation
      */
     private Object execute(GroovyShell shell, File scriptFile) {
         try {
             return shell.evaluate(scriptFile);
         } catch (Exception e) {
-            throw new RuntimeException(String.format("Couldn't execute a script %s.",scriptFile),e);
+            throw new RuntimeException(String.format("Couldn't execute script %s.", scriptFile), e);
         }
     }
-    
+
     @Required
     public void setScriptFile(File scriptFile) {
         this.scriptFile = scriptFile;
     }
-    
+
     public File getScriptFile() {
         return scriptFile;
     }
