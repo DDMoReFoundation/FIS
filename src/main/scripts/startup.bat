6a1de6d62aea05bdb2226c00413290ea5bf3bd13
Richard Kaye <rkaye@mango-solutions.com>
2014-07-08 16:31:30 +0000

    DDMORE-729: Update startup scripts to remove registry access and correctly locate java!  Also renamed FIS to be SERVICE to bring the script in line with others.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -1,13 +1,13 @@
-SET FIS_HOME=%~dp0
+SET SERVICE_HOME=%~dp0
 
-IF %FIS_HOME:~-1%==\ SET FIS_HOME=%FIS_HOME:~0,-1%
+IF %SERVICE_HOME:~-1%==\ SET SERVICE_HOME=%SERVICE_HOME:~0,-1%
 
-CD %FIS_HOME%
+CD %SERVICE_HOME%
 
-SET PUBLISH_INPUTS=%FIS_HOME%\scripts\publishInputs.groovy
-SET RETRIEVE_OUTPUTS=%FIS_HOME%\scripts\retrieveOutputs.groovy
-SET READ_RESOURCE=%FIS_HOME%\scripts\readResource.groovy
-SET WRITE_RESOURCE=%FIS_HOME%\scripts\writeResource.groovy
+SET PUBLISH_INPUTS=%SERVICE_HOME%\scripts\publishInputs.groovy
+SET RETRIEVE_OUTPUTS=%SERVICE_HOME%\scripts\retrieveOutputs.groovy
+SET READ_RESOURCE=%SERVICE_HOME%\scripts\readResource.groovy
+SET WRITE_RESOURCE=%SERVICE_HOME%\scripts\writeResource.groovy
 
 SET params=-Dfis.publishInputs="%PUBLISH_INPUTS%"
 SET params=-Dfis.retrieveOutputs="%RETRIEVE_OUTPUTS%" %params%
@@ -16,11 +16,25 @@ SET params=-Dfis.writeResource="%WRITE_RESOURCE%" %params%
 SET params=-Dnonmem.setup.script="%~dp0\..\setup.bat" %params%
 SET params=-Dconverter.toolbox.executable="%~dp0\..\converter-toolbox-bundle-0.0.1-SNAPSHOT\convert.bat" %params%
 
-reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul
-if %errorlevel%==0 (
-	set TRAINING_JAVA_HOME=%FIS_HOME%\..\MDL_IDE\x86\jre
+if "%PROCESSOR_ARCHITECTURE%" == "x86" ( 
+    if not defined PROCESSOR_ARCHITEW6432 (
+		set TRAINING_JAVA_HOME=%SERVICE_HOME%\..\MDL_IDE\x86\jre
+	) else (
+		set TRAINING_JAVA_HOME=%SERVICE_HOME%\..\MDL_IDE\x86_64\jre
+	)
 ) else (
-	set TRAINING_JAVA_HOME=%FIS_HOME%\..\MDL_IDE\x86_64\jre
+	set TRAINING_JAVA_HOME=%SERVICE_HOME%\..\MDL_IDE\x86_64\jre
 )
 
-%TRAINING_JAVA_HOME%\bin\java %params% -Dmango.mif.invoker.shell="cmd.exe /C" -Dmango.mif.invoker.tmp.file.ext=".bat" -DFIS_HOME="%FIS_HOME%" -jar %FIS_HOME%\fis.jar
+IF EXIST "%TRAINING_JAVA_HOME%\bin\java.exe" (
+    echo Using Java from SEE
+    SET JAVA_CMD=%TRAINING_JAVA_HOME%\bin\java
+) ELSE IF EXIST "%JAVA_HOME%\bin\java.exe" (
+    echo Using Java from JAVA_HOME environment variable
+    SET JAVA_CMD=%JAVA_HOME%\bin\java
+) ELSE (
+    echo Falling beck to using Java from path; it will fail if Java is not installed
+    SET JAVA_CMD=java
+)
+
+%JAVA_CMD% %params% -Dmango.mif.invoker.shell="cmd.exe /C" -Dmango.mif.invoker.tmp.file.ext=".bat" -DFIS_HOME="%SERVICE_HOME%" -jar %SERVICE_HOME%\fis.jar
