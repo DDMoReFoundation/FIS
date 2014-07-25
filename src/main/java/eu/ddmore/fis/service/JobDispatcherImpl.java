dbbdf44195783c669f9e68ba5856a719f655c359
Matthew Wise <mwise@mango-solutions.com>
2014-07-25 10:50:56 +0000

    DDMORE-800: Only provide CONVERTER_TOOLBOX_PATH on the Execution Request for MIF if it is actually a specified parameter of the resolved Command Execution Target.

--- src/main/java/eu/ddmore/fis/service/JobDispatcherImpl.java
+++ src/main/java/eu/ddmore/fis/service/JobDispatcherImpl.java
@@ -16,67 +16,68 @@ import eu.ddmore.fis.domain.LocalJobStatus;
  * Default implementation of {@link JobDispatcher}
  */
 public class JobDispatcherImpl implements JobDispatcher {
-    
-    private MIFHttpRestClient mifClient;
-    private String mifUserName = "tel-user";
-    private JobResourceProcessor jobResourcePublisher;
-    private CommandRegistry commandRegistry;
-    
-    public LocalJob dispatch(LocalJob job) {
-        
-        // Invoke the publishInputs Groovy script
-        LocalJob publishedJob = jobResourcePublisher.process(job);
-        
-        if (publishedJob.getStatus() != LocalJobStatus.FAILED) {
-            // Only continue if the pre-processing was successful
-            CommandExecutionTarget commandTarget = commandRegistry.resolveExecutionTargetFor(publishedJob.getCommand());
-            ExecutionRequest executionRequest = buildExecutionRequest(publishedJob, commandTarget);
-            mifClient.executeJob(executionRequest);
-        }
-        
-        return publishedJob;
-    }
 
-    @VisibleForTesting
-    ExecutionRequest buildExecutionRequest(LocalJob job, CommandExecutionTarget commandTarget) {
-        Preconditions.checkNotNull(job, "Job can't be null");
-        Preconditions.checkNotNull(commandTarget, "Command Target can't be null");
-        ExecutionRequestBuilder requestBuilder = new ExecutionRequestBuilder().setRequestId(job.getId())
-                .setName("FIS Service Job").setExecutionType(commandTarget.getExecutionType())
-                .setExecutionFile(job.getControlFile()).setCommand(commandTarget.getToolExecutablePath())
-                .setSubmitAsUserMode(false).setUserName(mifUserName);
-        requestBuilder.addAttribute("EXECUTION_HOST_FILESHARE", job.getWorkingDirectory());
-        requestBuilder.addAttribute("EXECUTION_HOST_FILESHARE_REMOTE", job.getWorkingDirectory());
-        requestBuilder.addAttribute("CONVERTER_TOOLBOX_PATH", commandTarget.getConverterToolboxPath());
-        requestBuilder.setSubmitHostPreamble(commandTarget.getEnvironmentSetupScript());
-        requestBuilder.setExecutionParameters(job.getCommandParameters());
-        return requestBuilder.getExecutionRequest();
-    }
-    
-    public MIFHttpRestClient getMifClient() {
-        return mifClient;
-    }
+	private MIFHttpRestClient mifClient;
+	private String mifUserName = "tel-user";
+	private JobResourceProcessor jobResourcePublisher;
+	private CommandRegistry commandRegistry;
 
-    @Required
-    public void setMifClient(MIFHttpRestClient mifClient) {
-        this.mifClient = mifClient;
-    }
+	public LocalJob dispatch(LocalJob job) {
 
-    public JobResourceProcessor getJobResourcePublisher() {
-        return jobResourcePublisher;
-    }
+		// Invoke the publishInputs Groovy script
+		LocalJob publishedJob = jobResourcePublisher.process(job);
 
-    @Required
-    public void setJobResourcePublisher(JobResourceProcessor jobResourcePublisher) {
-        this.jobResourcePublisher = jobResourcePublisher;
-    }
-    
-    @Required
-    public void setCommandRegistry(CommandRegistry commandRegistry) {
-        this.commandRegistry = commandRegistry;
-    }
-    
-    public CommandRegistry getCommandRegistry() {
-        return commandRegistry;
-    }
+		if (publishedJob.getStatus() != LocalJobStatus.FAILED) {
+			// Only continue if the pre-processing was successful
+			CommandExecutionTarget commandTarget = commandRegistry.resolveExecutionTargetFor(publishedJob.getCommand());
+			ExecutionRequest executionRequest = buildExecutionRequest(publishedJob, commandTarget);
+			mifClient.executeJob(executionRequest);
+		}
+
+		return publishedJob;
+	}
+
+	@VisibleForTesting
+	ExecutionRequest buildExecutionRequest(LocalJob job, CommandExecutionTarget commandTarget) {
+		Preconditions.checkNotNull(job, "Job can't be null");
+		Preconditions.checkNotNull(commandTarget, "Command Target can't be null");
+		ExecutionRequestBuilder requestBuilder = new ExecutionRequestBuilder().setRequestId(job.getId()).setName("FIS Service Job")
+		        .setExecutionType(commandTarget.getExecutionType()).setExecutionFile(job.getControlFile())
+		        .setCommand(commandTarget.getToolExecutablePath()).setSubmitAsUserMode(false).setUserName(mifUserName);
+		requestBuilder.addAttribute("EXECUTION_HOST_FILESHARE", job.getWorkingDirectory());
+		requestBuilder.addAttribute("EXECUTION_HOST_FILESHARE_REMOTE", job.getWorkingDirectory());
+		if (commandTarget.getConverterToolboxPath() != null) {
+			requestBuilder.addAttribute("CONVERTER_TOOLBOX_PATH", commandTarget.getConverterToolboxPath());
+		}
+		requestBuilder.setSubmitHostPreamble(commandTarget.getEnvironmentSetupScript());
+		requestBuilder.setExecutionParameters(job.getCommandParameters());
+		return requestBuilder.getExecutionRequest();
+	}
+
+	public MIFHttpRestClient getMifClient() {
+		return mifClient;
+	}
+
+	@Required
+	public void setMifClient(MIFHttpRestClient mifClient) {
+		this.mifClient = mifClient;
+	}
+
+	public JobResourceProcessor getJobResourcePublisher() {
+		return jobResourcePublisher;
+	}
+
+	@Required
+	public void setJobResourcePublisher(JobResourceProcessor jobResourcePublisher) {
+		this.jobResourcePublisher = jobResourcePublisher;
+	}
+
+	@Required
+	public void setCommandRegistry(CommandRegistry commandRegistry) {
+		this.commandRegistry = commandRegistry;
+	}
+
+	public CommandRegistry getCommandRegistry() {
+		return commandRegistry;
+	}
 }
