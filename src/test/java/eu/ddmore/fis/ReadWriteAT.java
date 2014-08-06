49787b6c583cb01168d339d488e7072eb777f97b
Matthew Wise <mwise@mango-solutions.com>
2014-08-06 09:39:50 +0000

    DDMORE-736: Add test to FIS's ReadWriteAT: Write test for Prolactin JSON - this silently fails when run via TEL console - but since this new test passes it eliminates FIS or the converter being the problem.

--- src/test/java/eu/ddmore/fis/ReadWriteAT.java
+++ src/test/java/eu/ddmore/fis/ReadWriteAT.java
@@ -18,64 +18,90 @@ import eu.ddmore.fis.domain.WriteMdlRequest;
 import eu.ddmore.fis.domain.WriteMdlResponse;
 
 /**
- * Verifies that standalone service fulfils functional requirements
+ * Verifies that standalone service fulfils functional requirements.
  */
 public class ReadWriteAT extends SystemPropertiesAware {
 
-    private static final String TEST_DATA_DIR = "/eu/ddmore/testdata/models/%s/ThamCCR2008/";
-    private static final String MDL_FILE_NAME = "tumour_size_25June2014_OAM.mdl";
-    private static final String JSON_FILE_NAME = "tumour_size_25June2014_OAM.json";
+	private static final String TEST_DATA_DIR = "/eu/ddmore/testdata/models/%s/ThamCCR2008/";
+	private static final String MDL_FILE_NAME = "tumour_size_25June2014_OAM.mdl";
+	private static final String JSON_FILE_NAME = "tumour_size_25June2014_OAM.json";
 
-    private static final URL MDL_FILE_URL = ReadWriteAT.class.getResource(String.format(TEST_DATA_DIR, "mdl") + MDL_FILE_NAME);
-    private static final URL JSON_FILE_URL = ReadWriteAT.class.getResource(String.format(TEST_DATA_DIR, "json") + JSON_FILE_NAME);
+	private static final URL MDL_FILE_URL = ReadWriteAT.class.getResource(String.format(TEST_DATA_DIR, "mdl") + MDL_FILE_NAME);
+	private static final URL JSON_FILE_URL = ReadWriteAT.class.getResource(String.format(TEST_DATA_DIR, "json") + JSON_FILE_NAME);
 
-    // This is where the output from FIS and MIF can be found
-    private static final File parentWorkingDir = new File("target", "ReadWriteAT_Test_Working_Dir");
-    private static final FISHttpRestClient fisClient = new FISHttpRestClient(System.getProperty("fis.url"));
+	// This is where the output from FIS and MIF can be found
+	private static final File parentWorkingDir = new File("target", "ReadWriteAT_Test_Working_Dir");
+	private final FISHttpRestClient fisClient = new FISHttpRestClient(System.getProperty("fis.url"));
 
+	@BeforeClass
+	public static void globalSetUp() throws Exception {
+		FileUtils.deleteDirectory(parentWorkingDir);
+		parentWorkingDir.mkdir();
+	}
 
-    @BeforeClass
-    public static void globalSetUp() throws Exception {
-        FileUtils.deleteDirectory(parentWorkingDir);
-        parentWorkingDir.mkdir();
-    }
+	@Test
+	public void shouldCorrectlyReadMdlFile() throws IOException {
+		final File workingDir = new File(parentWorkingDir, "ReadMdlFile");
+		workingDir.mkdir();
 
-    @Test
-    public void shouldCorrectlyReadMdlFile() throws IOException {
-        final File workingDir = new File(parentWorkingDir, "ReadMdlFile");
-        workingDir.mkdir();
+		final File mdlFile = new File(workingDir, MDL_FILE_NAME);
+		FileUtils.copyURLToFile(MDL_FILE_URL, mdlFile);
 
-        final File mdlFile = new File(workingDir, MDL_FILE_NAME);
-        FileUtils.copyURLToFile(MDL_FILE_URL, mdlFile);
+		final String mdlFileFullPath = mdlFile.getAbsolutePath().replace('\\', '/');
+		String jsonFormat = fisClient.readMdl(mdlFileFullPath);
 
-        final String mdlFileFullPath = mdlFile.getAbsolutePath().replace('\\', '/');
-        String jsonFormat = fisClient.readMdl(mdlFileFullPath);
+		final File jsonFile = new File(workingDir, JSON_FILE_NAME + ".tmp");
+		FileUtils.copyURLToFile(JSON_FILE_URL, jsonFile);
+		String expected = FileUtils.readFileToString(jsonFile);
+		assertEquals("MDL file should be correctly read.", expected, jsonFormat);
 
-        final File jsonFile = new File(workingDir, JSON_FILE_NAME + ".tmp");
-        FileUtils.copyURLToFile(JSON_FILE_URL, jsonFile);
-        String expected = FileUtils.readFileToString(jsonFile);
-        assertEquals("MDL file should be correctly read.", expected, jsonFormat);
+		final String jsonFileFullPath = mdlFileFullPath.replace(MDL_FILE_NAME, JSON_FILE_NAME);
+		final File f = new File(jsonFileFullPath);
+		assertTrue("Temporary JSON file should not be present.", !f.exists());
 
-        final String jsonFileFullPath = mdlFileFullPath.replace(MDL_FILE_NAME, JSON_FILE_NAME);
-        final File f = new File(jsonFileFullPath);
-        assertTrue("Temporary JSON file should not be present.", !f.exists());
+	}
 
-    }
+	@Test
+	public void shouldCorrectlyWriteMdlFile_TumourSize() throws IOException {
+		final File workingDir = new File(parentWorkingDir, "WriteMdlFile_TumourSize");
+		workingDir.mkdir();
 
-    @Test
-    public void shouldCorrectlyWriteMdlFile() throws IOException {
-        final File workingDir = new File(parentWorkingDir, "WriteMdlFile");
-        workingDir.mkdir();
+		final File jsonFile = new File(workingDir, JSON_FILE_NAME);
+		FileUtils.copyURLToFile(JSON_FILE_URL, jsonFile);
 
-        final File jsonFile = new File(workingDir, JSON_FILE_NAME);
-        FileUtils.copyURLToFile(JSON_FILE_URL, jsonFile);
+		WriteMdlRequest writeReq = new WriteMdlRequest();
+		writeReq.setFileContent(FileUtils.readFileToString(jsonFile));
+		writeReq.setFileName(workingDir.getAbsolutePath() + "/" + MDL_FILE_NAME);
 
-        WriteMdlRequest writeReq = new WriteMdlRequest();
-        writeReq.setFileContent(FileUtils.readFileToString(jsonFile));
-        writeReq.setFileName(workingDir.getAbsolutePath() + "/" + MDL_FILE_NAME);
-        WriteMdlResponse writeResp = fisClient.writeMdl(writeReq);
-        assertEquals("MDL file should be correctly written.", "Successful", writeResp.getStatus());
+		// Call the method under test
+		final WriteMdlResponse writeResp = this.fisClient.writeMdl(writeReq);
 
-    }
+		assertTrue("MDL output file should have been created", new File(workingDir, MDL_FILE_NAME).exists());
+		assertEquals("Success status should be reported from the service", "Successful", writeResp.getStatus());
+	}
+
+	@Test
+	public void shouldCorrectlyWriteMdlFile_Prolactin() throws IOException {
+		final File workingDir = new File(parentWorkingDir, "WriteMdlFile_Prolactin");
+		workingDir.mkdir();
+
+		final String MDL_FILE_NAME = "ex_model7_prolactin_25June2014_OAM.mdl";
+		final String JSON_FILE_NAME = "ex_model7_prolactin_25June2014_OAM.json";
+
+		final URL JSON_FILE_URL = ReadWriteAT.class.getResource("/eu/ddmore/testdata/models/json/prolactin/" + JSON_FILE_NAME);
+
+		final File jsonFile = new File(workingDir, JSON_FILE_NAME);
+		FileUtils.copyURLToFile(JSON_FILE_URL, jsonFile);
+
+		WriteMdlRequest writeReq = new WriteMdlRequest();
+		writeReq.setFileContent(FileUtils.readFileToString(jsonFile));
+		writeReq.setFileName(workingDir.getAbsolutePath() + "/" + MDL_FILE_NAME);
+
+		// Call the method under test
+		final WriteMdlResponse writeResp = this.fisClient.writeMdl(writeReq);
+
+		assertTrue("MDL output file should have been created", new File(workingDir, MDL_FILE_NAME).exists());
+		assertEquals("Success status should be reported from the service", "Successful", writeResp.getStatus());
+	}
 
 }
