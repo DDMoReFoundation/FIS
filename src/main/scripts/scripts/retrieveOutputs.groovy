893ca8fe63182e7f0d67fed647e6f118c46c0c22
Matthew Wise <mwise@mango-solutions.com>
2014-09-30 12:09:23 +0000

    DDMORE-812: Write out an error if outputFilenamesRegex is not configured on a MIF Connector.

--- src/main/scripts/scripts/retrieveOutputs.groovy
+++ src/main/scripts/scripts/retrieveOutputs.groovy
@@ -9,22 +9,14 @@ LocalJob job = binding.getVariable("job");
 File workingDir = new File(job.getWorkingDirectory())
 File mifWorkingDir = new File(workingDir, job.getId());
 
-Pattern outputFilenamesRegex = Pattern.compile(job.getOutputFilenamesRegex())
-
-// Copy back any files that match the filename regular expression and which aren't "hidden" e.g. .MIF directory
-FileUtils.copyDirectory(mifWorkingDir, workingDir, new FileFilter() {
-            boolean accept(File file) {
-				return (file.getName().matches(outputFilenamesRegex) && !file.getName().startsWith("."));
-            }
-        });
 
 // Ensure that the FIS metadata directory is created
 File fisMetadataDir = new File(workingDir,".fis");
 fisMetadataDir.mkdir();
 
 File mifMetadataDir = new File(mifWorkingDir,".MIF");
-File mifStdErr=new File(mifMetadataDir, "MIF.stderr");
-if(mifStdErr.exists()) {
+File mifStdErr = new File(mifMetadataDir, "MIF.stderr");
+if (mifStdErr.exists()) {
     FileUtils.copyFile(mifStdErr,new File(fisMetadataDir, "stderr"));
 } else {
     println "MIF.stderr file did not exist"
@@ -35,4 +27,20 @@ if(mifStdErr.exists()) {
     FileUtils.copyFile(mifStdOut,new File(fisMetadataDir, "stdout"));
 } else {
     println "MIF.stdout file did not exist"
+}
+
+
+// Copy output files
+if (job.getOutputFilenamesRegex() != null) {
+	
+	final Pattern outputFilenamesRegex = Pattern.compile(job.getOutputFilenamesRegex())
+
+	// Copy back any files that match the filename regular expression and which aren't "hidden" e.g. .MIF directory
+	FileUtils.copyDirectory(mifWorkingDir, workingDir, new FileFilter() {
+		boolean accept(File file) {
+			return (file.getName().matches(outputFilenamesRegex) && !file.getName().startsWith("."));
+		}
+	});
+} else {
+	FileUtils.write(new File(fisMetadataDir, "stderr"), "\n\nOutput Filenames Regular Expression was not specified in the MIF/TES Connector configuration; no output files will be copied back.\n", true); // true = append
 }
