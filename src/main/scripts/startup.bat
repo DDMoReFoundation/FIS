5ccf993b4ae7440426dedbe121cc7342288e71ce
Matthew Wise <mwise@mango-solutions.com>
2014-10-08 17:22:08 +0000

    DDMORE-861: Tidy up the derivation of the path to java slightly amongst the MIF, FIS and SEE startup scripts.

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -1,4 +1,6 @@
-REM Locations without trailing '\'
+@echo off
+
+REM  Locations without trailing '\'
 SET SERVICE_HOME=%~dp0
 IF %SERVICE_HOME:~-1%==\ SET SERVICE_HOME=%SERVICE_HOME:~0,-1%
 
@@ -15,17 +17,15 @@ SET params= -Dfis.publishInputs="%PUBLISH_INPUTS%" ^
  -Dfis.writeResource="%WRITE_RESOURCE%" ^
  -Dconverter.toolbox.executable="%SERVICE_HOME%\..\converter-toolbox-bundle\convert.bat"
 
-set TRAINING_JAVA_HOME=%SERVICE_HOME%\..\MDL_IDE\jre
-
-IF EXIST "%TRAINING_JAVA_HOME%\bin\java.exe" (
-    echo Using Java from SEE
-    SET JAVA_CMD="%TRAINING_JAVA_HOME%\bin\java"
-) ELSE IF EXIST "%JAVA_HOME%\bin\java.exe" (
-    echo Using Java from JAVA_HOME environment variable
-    SET JAVA_CMD="%JAVA_HOME%\bin\java"
-) ELSE (
-    echo Falling beck to using Java from path; it will fail if Java is not installed
-    SET JAVA_CMD=java
+IF NOT DEFINED JAVA_CMD (
+    echo FIS is executing in standalone mode, outside of SEE, which would have set JAVA_CMD
+    IF EXIST "%JAVA_HOME%\bin\java.exe" (
+        echo Using Java from JAVA_HOME environment variable
+        SET JAVA_CMD="%JAVA_HOME%\bin\java"
+    ) ELSE (
+        echo Falling beck to using Java from system path; this will fail if Java is not installed
+        SET JAVA_CMD=java
+    )
 )
 
 %JAVA_CMD% %params% -DFIS_HOME="%SERVICE_HOME%" -jar "%SERVICE_HOME%"\fis.jar
