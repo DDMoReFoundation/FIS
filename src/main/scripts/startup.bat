c302259e0b3cf58e0e9d017e6360e4208c826a15
Matthew Wise <mwise@mango-solutions.com>
2014-10-17 17:07:36 +0000

    DDMORE-861: Redirect stdin into the powershell command in FIS startup.bat in order to prevent it hanging when there's no command window i.e. when being launched from TEL.startServer().

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -2,7 +2,7 @@
 
 REM  This Windows Powershell command (built in to Windows 7) is merely a 'nice-to-have' that
 REM  sets a large console buffer size in order that the entire output can be scrolled around.
-powershell -command "&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.height=2000;$W.buffersize=$B;}" 1>NUL 2>NUL
+powershell -command "&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.height=2000;$W.buffersize=$B;}" <NUL 1>NUL 2>NUL
 
 REM  Locations without trailing '\'
 SET SERVICE_HOME=%~dp0
