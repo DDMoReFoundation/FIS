a1f9b8f258698df73b08d8672523f8b6157119db
Matthew Wise <mwise@mango-solutions.com>
2015-01-22 13:03:47 +0000

    DDMORE-986: Update MDL & JSON versions to 6.0.7 in FIS.

--- src/main/scripts/scripts/MdlToPharmML.groovy
+++ src/main/scripts/scripts/MdlToPharmML.groovy
@@ -14,7 +14,7 @@ final class MdlToPharmML extends AbstractConverterWrapper {
     private String pharmmlFileExt
 
     MdlToPharmML(final Binding binding) {
-        super(binding, "MDL", "5.1.6-interop", "PharmML", "0.3.1", binding.getVariable("fis.pharmml.ext")) // TODO: Remove the hard-coding of these source and target converter versions
+        super(binding, "MDL", "6.0.7", "PharmML", "0.3.1", binding.getVariable("fis.pharmml.ext")) // TODO: Remove the hard-coding of these source and target converter versions
         this.pharmmlFileExt = binding.getVariable("fis.pharmml.ext")
     }
 
