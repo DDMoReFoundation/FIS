97397856fc7feb9442bf3bc69f20f3d6cf07f3d4
matsrogalski <mrogalski@mango-solutions.com>
2016-04-25 17:08:36 +0100

    DDMORE-1640: Don't expose user info repository via REST.

--- src/main/java/eu/ddmore/fis/repository/UserInfoRepository.java
+++ src/main/java/eu/ddmore/fis/repository/UserInfoRepository.java
@@ -4,10 +4,12 @@
 package eu.ddmore.fis.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import eu.ddmore.fis.domain.UserInfo;
 
 @Repository
+@RestResource(exported=false)
 public interface UserInfoRepository extends CrudRepository<UserInfo, Long>{
 }
